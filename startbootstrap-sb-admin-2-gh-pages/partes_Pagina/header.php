26c9271c51d8e1c33852123d52545f98c9484353
Gerardo <gerardocorica10@gmail.com>
2024-07-08 09:55:13 -0300

    1.0

---
+++ startbootstrap-sb-admin-2-gh-pages/partes_Pagina/header.php
@@ -0,0 +1,3 @@
+<header class="conteneiner-fluid bg-gradient-primary d-flex justify-content-center fw-bold">
+    <a class="text-light m-0 mt-2 mb-2 fs-5" id="pet_header" href="index.php">PET SALUD</a>
+</header>
