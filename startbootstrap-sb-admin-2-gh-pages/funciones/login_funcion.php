3c2e1ed4d6a8ee8fc3bef30a9946e39fc997ce83
adrianquilici <adrianquilici@gmail.com>
2024-07-11 19:56:48 -0300

    bbb

--- startbootstrap-sb-admin-2-gh-pages/funciones/login_funcion.php
+++ startbootstrap-sb-admin-2-gh-pages/funciones/login_funcion.php
@@ -2,31 +2,36 @@
 function DatosLogin($vUsuario, $vClave, $vConexion){
     $Usuario=array();
 
+
     $SQL="SELECT p.*
-        FROM 
-            persona as p, 
+        FROM
+            persona as p,
             DATOS_DE_CONTACTO as d
-        WHERE 
-            P.ID_DATOS_DE_CONTACTO = d.ID_DATOS_DE_CONTACTO 
-        AND 
-            d.CORREO_ELECTRONICO='$vUsuario' 
-        AND 
+        WHERE
+            P.ID_DATOS_DE_CONTACTO = d.ID_DATOS_DE_CONTACTO
+        AND
+            d.CORREO_ELECTRONICO='$vUsuario'
+        AND
             p.CONTRASENA= '$vClave'
     ";
 
+
     $rs=mysqli_query($vConexion, $SQL);
 
+
     $data= mysqli_fetch_array($rs);
+
 
     if(!empty($data)){
             $Usuario['Nombre']=$data['NOMBRE'];
             $Usuario['Apellido']=$data['APELLIDO'];
-            $Usuario['DNI_Persona']=$data['DNI'];
-        }else{
             $Usuario['Estado']=$data['ESTADO_PERSONA'];
+       
     }
     return $Usuario;
 }
+
+
 
 
 ?>
