bb4e6e051ec0a23084303e9cef87556bde419783
Gerardo <gerardocorica10@gmail.com>
2024-12-10 14:01:05 -0300

    si o si

--- petsalud-v3_0.sql
+++ petsalud-v3_0.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 05-12-2024 a las 02:42:07
+-- Tiempo de generación: 09-12-2024 a las 10:21:25
 -- Versión del servidor: 5.5.27
 -- Versión de PHP: 5.4.7
 
@@ -37,6 +37,7 @@ CREATE TABLE IF NOT EXISTS `animal` (
   `ESTADO_CASTRACION` int(11) NOT NULL,
   PRIMARY KEY (`ID_ANIMAL`),
   UNIQUE KEY `ANIMAL_PK` (`ID_ANIMAL`),
+  UNIQUE KEY `CODIGO_ANIMAL` (`CODIGO_ANIMAL`),
   KEY `RELATION_77_FK` (`ID_TIPO_ANIMAL`),
   KEY `RELATION_78_FK` (`ID_RAZA_ANIMAL`),
   KEY `RELATION_79_FK` (`ID_ROL_ANIMAL`)
@@ -47,23 +48,10 @@ CREATE TABLE IF NOT EXISTS `animal` (
 --
 
 INSERT INTO `animal` (`ID_ANIMAL`, `CODIGO_ANIMAL`, `ID_TIPO_ANIMAL`, `ID_RAZA_ANIMAL`, `ID_ROL_ANIMAL`, `NOMBRE_ANIMAL`, `ESTADO_ANIMAL`, `ESTADO_CASTRACION`) VALUES
-(2, 0, 2, 4, 1, 'otto', 0, 0),
-(3, 0, 2, 4, 1, 'otto', 0, 0),
-(4, 0, 2, 4, 1, 'otto', 0, 0),
-(5, 0, 2, 4, 1, 'otto', 0, 0),
-(6, 0, 2, 4, 1, 'otto', 0, 0),
-(7, 0, 2, 3, 1, 'otto', 1, 0),
-(8, 0, 1, 1, 2, 'toto', 1, 0),
-(9, 0, 1, 2, 1, 'pepe', 0, 0),
-(10, 0, 3, 7, 4, 'pepe', 1, 0),
-(11, 0, 2, 3, 1, 'pepe', 1, 0),
-(12, 0, 1, 1, 1, 'pepe', 1, 0),
-(13, 0, 2, 3, 1, 'otto', 1, 0),
-(14, 0, 2, 5, 1, 'otto', 1, 0),
-(15, 0, 1, 1, 1, 'pepe', 0, 0),
-(16, 0, 1, 1, 1, 'rufo', 0, 0),
-(17, 0, 1, 1, 1, 'rufito', 0, 0),
-(18, 0, 1, 3, 1, 'panky', 1, 0),
+(15, 0, 1, 1, 1, 'pepe', 1, 0),
+(16, 1, 1, 1, 1, 'rufo', 0, 0),
+(17, 2, 1, 1, 1, 'rufito', 1, 0),
+(18, 3, 1, 3, 1, 'panky', 1, 0),
 (19, 549, 1, 3, 1, 'panky luisss', 1, 0),
 (20, 550, 1, 3, 2, 'carloncho bb', 1, 0),
 (21, 12345, 1, 2, 1, 'pepe', 0, 0),
@@ -84,7 +72,7 @@ INSERT INTO `animal` (`ID_ANIMAL`, `CODIGO_ANIMAL`, `ID_TIPO_ANIMAL`, `ID_RAZA_A
 (37, 12359, 2, 4, 7, 'reneee', 1, 0),
 (38, 12360, 2, 4, 8, 'reneeee', 0, 0),
 (39, 12361, 1, 1, 1, 'pio', 1, 0),
-(40, 548, 2, 4, 7, 'sofi', 1, 1);
+(40, 548, 2, 4, 7, 'sofi', 1, 0);
 
 -- --------------------------------------------------------
 
@@ -119,7 +107,7 @@ INSERT INTO `area_municipal` (`ID_AREA_MUNICIPAL`, `DESCRIPCION_AREA_MUNICIPAL`)
 --
 
 CREATE TABLE IF NOT EXISTS `campana` (
-  `ID_CAMPANA` int(11) NOT NULL,
+  `ID_CAMPANA` int(11) NOT NULL AUTO_INCREMENT,
   `ID_TIPO_DE_CAMPANA` int(11) NOT NULL,
   `ID_TRABAJADOR_MUNICIPAL` int(11) NOT NULL,
   `DESCRIPCION_CAMPANA` varchar(300) NOT NULL,
@@ -127,11 +115,25 @@ CREATE TABLE IF NOT EXISTS `campana` (
   `FECHA_DE_INICIO` date NOT NULL,
   `FECHA_DE_FIN` date NOT NULL,
   `ESTADO_CAMPANA` tinyint(1) NOT NULL,
+  `ID_ESPECIE` int(11) NOT NULL,
+  `ID_VETERINARIO` int(11) NOT NULL,
   PRIMARY KEY (`ID_CAMPANA`),
   UNIQUE KEY `CAMPANA_PK` (`ID_CAMPANA`),
   KEY `RELATION_154_FK` (`ID_TIPO_DE_CAMPANA`),
-  KEY `RELATION_155_FK` (`ID_TRABAJADOR_MUNICIPAL`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  KEY `RELATION_155_FK` (`ID_TRABAJADOR_MUNICIPAL`),
+  KEY `FK_Campana_Especie` (`ID_ESPECIE`),
+  KEY `FK_Campana_Veterinario` (`ID_VETERINARIO`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=18 ;
+
+--
+-- Volcado de datos para la tabla `campana`
+--
+
+INSERT INTO `campana` (`ID_CAMPANA`, `ID_TIPO_DE_CAMPANA`, `ID_TRABAJADOR_MUNICIPAL`, `DESCRIPCION_CAMPANA`, `FECHA_DE_CREACION`, `FECHA_DE_INICIO`, `FECHA_DE_FIN`, `ESTADO_CAMPANA`, `ID_ESPECIE`, `ID_VETERINARIO`) VALUES
+(14, 2, 3, 'castracion gatos', '2024-12-06', '2024-12-09', '2024-12-11', 1, 2, 6),
+(15, 1, 3, 'Vacunacion perros', '2024-12-06', '2024-12-10', '2024-12-11', 1, 1, 1),
+(16, 1, 3, 'Vacunacion perros 2', '2024-12-07', '2024-12-13', '2024-12-15', 0, 1, 6),
+(17, 1, 3, 'pipip', '2024-12-08', '2025-01-01', '2025-01-02', 1, 1, 6);
 
 -- --------------------------------------------------------
 
@@ -146,7 +148,7 @@ CREATE TABLE IF NOT EXISTS `ciudad` (
   PRIMARY KEY (`ID_CIUDAD`),
   UNIQUE KEY `CIUDAD_PK` (`ID_CIUDAD`),
   KEY `RELATION_47_FK` (`ID_PROVINCIA`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=43 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=46 ;
 
 --
 -- Volcado de datos para la tabla `ciudad`
@@ -193,7 +195,10 @@ INSERT INTO `ciudad` (`ID_CIUDAD`, `ID_PROVINCIA`, `NOMBRE_CIUDAD`) VALUES
 (39, 1, 'aaa'),
 (40, 2, 'rio tercero'),
 (41, 1, 'rosario'),
-(42, 1, 'rosario');
+(42, 1, 'rosario'),
+(43, 18, 'tulim'),
+(44, 19, ''),
+(45, 19, 'san luis');
 
 -- --------------------------------------------------------
 
@@ -208,7 +213,7 @@ CREATE TABLE IF NOT EXISTS `datos_de_contacto` (
   `RED_SOCIAL` varchar(250) DEFAULT NULL,
   PRIMARY KEY (`ID_DATOS_DE_CONTACTO`),
   UNIQUE KEY `DATOS_DE_CONTACTO_PK` (`ID_DATOS_DE_CONTACTO`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=37 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=40 ;
 
 --
 -- Volcado de datos para la tabla `datos_de_contacto`
@@ -250,7 +255,10 @@ INSERT INTO `datos_de_contacto` (`ID_DATOS_DE_CONTACTO`, `TELEFONO`, `CORREO_ELE
 (33, '01234567890', 'aa@gmail.com', ''),
 (34, '01234567890', 'bb@gmail.com', ''),
 (35, '01234567890', 'cc@gmail.com', ''),
-(36, '01234567890', 'dd@gmail.com', '');
+(36, '01234567890', 'dd@gmail.com', ''),
+(37, '1234567890', 'fran@gmail.com', ''),
+(38, '1234567890', 'medy@gmail.com', ''),
+(39, '0123456789', 'medi@gmail.com', '');
 
 -- --------------------------------------------------------
 
@@ -267,7 +275,7 @@ CREATE TABLE IF NOT EXISTS `direccion` (
   PRIMARY KEY (`ID_DIRECCION`),
   UNIQUE KEY `DIRECCION_PK` (`ID_DIRECCION`),
   KEY `RELATION_46_FK` (`ID_CIUDAD`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=40 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=43 ;
 
 --
 -- Volcado de datos para la tabla `direccion`
@@ -312,7 +320,10 @@ INSERT INTO `direccion` (`ID_DIRECCION`, `ID_CIUDAD`, `NOMBRE_CALLE`, `NUMERO`, 
 (36, 39, 'antartida argentina', 123, 0),
 (37, 40, 'antartida argentina', 78, 0),
 (38, 41, 'antartida argentina', 456, 0),
-(39, 42, 'antartida argentina', 789, 1);
+(39, 42, 'antartida argentina', 789, 1),
+(40, 43, 'antartida argentina', 123, 1),
+(41, 44, 'san martin', 123, 1),
+(42, 45, 'belgrano 23', 123, 1);
 
 -- --------------------------------------------------------
 
@@ -327,7 +338,7 @@ CREATE TABLE IF NOT EXISTS `dueno_animal` (
   PRIMARY KEY (`ID_DUENO_ANIMAL`),
   UNIQUE KEY `DUENO_ANIMAL_PK` (`ID_DUENO_ANIMAL`),
   KEY `RELATION_81_FK` (`DNI`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
 
 --
 -- Volcado de datos para la tabla `dueno_animal`
@@ -336,7 +347,8 @@ CREATE TABLE IF NOT EXISTS `dueno_animal` (
 INSERT INTO `dueno_animal` (`ID_DUENO_ANIMAL`, `ESTADO_DUENO_ANIMAL`, `DNI`) VALUES
 (1, 0, 11112222),
 (2, 0, 11118888),
-(3, 1, 11119999);
+(3, 1, 11119999),
+(4, 1, 11111116);
 
 -- --------------------------------------------------------
 
@@ -572,12 +584,12 @@ INSERT INTO `nacionalidad` (`ID_NACIONALIDAD`, `PAIS`) VALUES
 (9, 'El Salvador'),
 (10, 'Guatemala'),
 (11, 'Honduras'),
-(12, 'México'),
+(12, 'Mexico'),
 (13, 'Nicaragua'),
-(14, 'Panamá'),
+(14, 'Panama'),
 (15, 'Paraguay'),
-(16, 'Perú'),
-(17, 'República Dominicana'),
+(16, 'Peru'),
+(17, 'Republica Dominicana'),
 (18, 'Uruguay'),
 (19, 'Venezuela');
 
@@ -650,7 +662,10 @@ INSERT INTO `persona` (`FECHA_DE_NACIMIENTO`, `ID_NACIONALIDAD`, `NACIONALIDAD`,
 ('2024-07-22', 1, 'Argentina', 'hijo de wiliamsss', 'facundoo', 'rabitii', 3, 11111113, 20, 1, 17, 123456, 3),
 ('2024-07-22', 1, 'Argentina', 'hijaaa de wiliamsss', 'ss', 'rabitii', 3, 11111114, 22, 1, 19, 123456, 3),
 ('2022-12-18', 18, 'Uruguay', 'hermano de paooooooo', 'jose primero', 'giorsettiiiiii', 3, 11111115, 23, 1, 20, 123456, 3),
+('1990-01-01', NULL, '1', '', 'franco', 'colapinto', 3, 11111116, 40, 1, 37, 123456, 4),
+('1980-02-02', NULL, '8', '', 'edgar', 'medina', 3, 11111117, 41, 1, 38, 123456, 2),
 ('1986-08-17', 1, 'Argentina', 'hola soy maria', 'maria', 'muni', 1, 11111118, 25, 1, 22, 123456, 2),
+('1980-02-01', NULL, '6', '', 'sebastian', 'medina', 3, 11111119, 42, 1, 39, 123456, 2),
 ('2006-09-26', 1, 'Argentina', 'adgrfasdgf', 'pepe', 'pepito', 3, 11111155, 31, 1, 28, 123456, 2),
 ('1960-01-01', NULL, 'Argentina', '', 'elina', 'lagorio', 1, 11112222, 32, 1, 29, 123456, 4),
 ('1985-01-01', NULL, 'Argentina', '', 'martin', 'bojanich', 3, 11113333, 33, 1, 30, 123456, 4),
@@ -897,7 +912,7 @@ CREATE TABLE IF NOT EXISTS `trabajador_municipal` (
   KEY `RELATION_125_FK` (`DNI`),
   KEY `RELATION_131_FK` (`ID_ROL_MUNICIPAL`),
   KEY `RELATION_137_FK` (`ID_AREA_MUNICIPAL`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=12 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=14 ;
 
 --
 -- Volcado de datos para la tabla `trabajador_municipal`
@@ -911,7 +926,9 @@ INSERT INTO `trabajador_municipal` (`ID_TRABAJADOR_MUNICIPAL`, `ID_ROL_MUNICIPAL
 (8, 1, 1, 1, 12345679),
 (9, 1, 1, 1, 11111118),
 (10, 3, 2, 1, 88888888),
-(11, 6, 7, 1, 11111155);
+(11, 6, 7, 1, 11111155),
+(12, 1, 6, 1, 11111117),
+(13, 6, 7, 1, 11111119);
 
 -- --------------------------------------------------------
 
@@ -928,7 +945,77 @@ CREATE TABLE IF NOT EXISTS `turnos` (
   `ESTADO_TURNO` tinyint(11) NOT NULL,
   PRIMARY KEY (`ID_TURNO`),
   KEY `FK_Turnos_Campana` (`ID_CAMPANA_TURNO`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=79 ;
+
+--
+-- Volcado de datos para la tabla `turnos`
+--
+
+INSERT INTO `turnos` (`ID_TURNO`, `ID_CAMPANA_TURNO`, `FECHA_TURNO`, `HORA_INICIO_TURNO`, `HORA_FIN_TURNO`, `ESTADO_TURNO`) VALUES
+(45, 14, '2024-12-09', '10:00:00', '12:00:00', 0),
+(46, 14, '2024-12-09', '12:30:00', '14:30:00', 0),
+(47, 14, '2024-12-10', '10:00:00', '12:00:00', 1),
+(48, 14, '2024-12-10', '12:30:00', '14:30:00', 0),
+(49, 14, '2024-12-11', '10:00:00', '12:00:00', 0),
+(50, 14, '2024-12-11', '12:30:00', '14:30:00', 0),
+(51, 15, '2024-12-10', '09:00:00', '09:10:00', 1),
+(52, 15, '2024-12-10', '09:15:00', '09:25:00', 0),
+(53, 15, '2024-12-10', '09:30:00', '09:40:00', 0),
+(54, 15, '2024-12-10', '09:45:00', '09:55:00', 0),
+(55, 15, '2024-12-10', '10:00:00', '10:10:00', 1),
+(56, 15, '2024-12-10', '10:15:00', '10:25:00', 0),
+(57, 15, '2024-12-11', '09:00:00', '09:10:00', 0),
+(58, 15, '2024-12-11', '09:15:00', '09:25:00', 0),
+(59, 15, '2024-12-11', '09:30:00', '09:40:00', 0),
+(60, 15, '2024-12-11', '09:45:00', '09:55:00', 0),
+(61, 15, '2024-12-11', '10:00:00', '10:10:00', 1),
+(62, 15, '2024-12-11', '10:15:00', '10:25:00', 0),
+(63, 16, '2024-12-13', '09:00:00', '09:15:00', 0),
+(64, 16, '2024-12-13', '09:20:00', '09:35:00', 0),
+(65, 16, '2024-12-13', '09:40:00', '09:55:00', 0),
+(66, 16, '2024-12-13', '10:00:00', '10:15:00', 0),
+(67, 16, '2024-12-14', '09:00:00', '09:15:00', 0),
+(68, 16, '2024-12-14', '09:20:00', '09:35:00', 0),
+(69, 16, '2024-12-14', '09:40:00', '09:55:00', 0),
+(70, 16, '2024-12-14', '10:00:00', '10:15:00', 0),
+(71, 16, '2024-12-15', '09:00:00', '09:15:00', 0),
+(72, 16, '2024-12-15', '09:20:00', '09:35:00', 0),
+(73, 16, '2024-12-15', '09:40:00', '09:55:00', 0),
+(74, 16, '2024-12-15', '10:00:00', '10:15:00', 0),
+(75, 17, '2025-01-01', '10:00:00', '10:10:00', 0),
+(76, 17, '2025-01-01', '10:15:00', '10:25:00', 0),
+(77, 17, '2025-01-02', '10:00:00', '10:10:00', 0),
+(78, 17, '2025-01-02', '10:15:00', '10:25:00', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `turnos_reservados`
+--
+
+CREATE TABLE IF NOT EXISTS `turnos_reservados` (
+  `ID_TURNO_RESERVADO` int(11) NOT NULL AUTO_INCREMENT,
+  `ID_TURNO` int(11) NOT NULL,
+  `ID_ANIMAL` int(11) NOT NULL,
+  `ID_DUENO_ANIMAL` int(11) NOT NULL,
+  `ID_CAMPANA` int(11) NOT NULL,
+  `ESTADO_TURNO_RESERVADO` int(11) NOT NULL,
+  PRIMARY KEY (`ID_TURNO_RESERVADO`),
+  KEY `FK_TURNO` (`ID_TURNO`),
+  KEY `FK_ANIMAL` (`ID_ANIMAL`),
+  KEY `FK_DUENO_ANIMAL` (`ID_DUENO_ANIMAL`),
+  KEY `FK_CAMPANA` (`ID_CAMPANA`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=10 ;
+
+--
+-- Volcado de datos para la tabla `turnos_reservados`
+--
+
+INSERT INTO `turnos_reservados` (`ID_TURNO_RESERVADO`, `ID_TURNO`, `ID_ANIMAL`, `ID_DUENO_ANIMAL`, `ID_CAMPANA`, `ESTADO_TURNO_RESERVADO`) VALUES
+(5, 51, 549, 3, 15, 1),
+(6, 55, 550, 3, 15, 1),
+(7, 61, 2, 2, 15, 1),
+(9, 47, 548, 1, 14, 1);
 
 -- --------------------------------------------------------
 
@@ -1005,10 +1092,12 @@ ALTER TABLE `animal`
 -- Filtros para la tabla `campana`
 --
 ALTER TABLE `campana`
-  ADD CONSTRAINT `FK_Campana_TrabajadorMunicipal` FOREIGN KEY (`ID_TRABAJADOR_MUNICIPAL`) REFERENCES `trabajador_municipal` (`ID_TRABAJADOR_MUNICIPAL`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK_Campana_Veterinario` FOREIGN KEY (`ID_VETERINARIO`) REFERENCES `veterinario` (`ID_VETERINARIO`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `campana_ibfk_1` FOREIGN KEY (`ID_TIPO_DE_CAMPANA`) REFERENCES `tipo_de_campana` (`ID_TIPO_DE_CAMPANA`),
   ADD CONSTRAINT `campana_ibfk_2` FOREIGN KEY (`ID_TRABAJADOR_MUNICIPAL`) REFERENCES `trabajador_municipal` (`ID_TRABAJADOR_MUNICIPAL`),
-  ADD CONSTRAINT `FK_Campana_TipoCampana` FOREIGN KEY (`ID_TIPO_DE_CAMPANA`) REFERENCES `tipo_de_campana` (`ID_TIPO_DE_CAMPANA`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `FK_Campana_Especie` FOREIGN KEY (`ID_ESPECIE`) REFERENCES `especie_animal` (`ID_TIPO_ANIMAL`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK_Campana_TipoCampana` FOREIGN KEY (`ID_TIPO_DE_CAMPANA`) REFERENCES `tipo_de_campana` (`ID_TIPO_DE_CAMPANA`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK_Campana_TrabajadorMunicipal` FOREIGN KEY (`ID_TRABAJADOR_MUNICIPAL`) REFERENCES `trabajador_municipal` (`ID_TRABAJADOR_MUNICIPAL`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Filtros para la tabla `ciudad`
@@ -1100,6 +1189,14 @@ ALTER TABLE `trabajador_municipal`
 --
 ALTER TABLE `turnos`
   ADD CONSTRAINT `FK_Turnos_Campana` FOREIGN KEY (`ID_CAMPANA_TURNO`) REFERENCES `campana` (`ID_CAMPANA`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `turnos_reservados`
+--
+ALTER TABLE `turnos_reservados`
+  ADD CONSTRAINT `FK_CAMPANA` FOREIGN KEY (`ID_CAMPANA`) REFERENCES `campana` (`ID_CAMPANA`),
+  ADD CONSTRAINT `FK_DUENO_ANIMAL` FOREIGN KEY (`ID_DUENO_ANIMAL`) REFERENCES `dueno_animal` (`ID_DUENO_ANIMAL`),
+  ADD CONSTRAINT `FK_TURNO` FOREIGN KEY (`ID_TURNO`) REFERENCES `turnos` (`ID_TURNO`);
 
 --
 -- Filtros para la tabla `vacunas`
